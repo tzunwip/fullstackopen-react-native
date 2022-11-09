f4b73fd4dd523ebee1cc0b9c3f3e95cce23494d6
Tzun Wong <tzun.wong@gmail.com>
2022-11-09 20:54:26 +0800

    initial commit rate-repository-app

---
+++ rate-repository-app/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:jsx-a11y/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {},
+  settings: { react: { version: "detect" } },
+};
