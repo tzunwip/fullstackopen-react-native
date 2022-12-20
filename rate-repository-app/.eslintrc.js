10f0743719980080015e66517b6a3541b5a690a6
Tzun Wong <tzun.wong@gmail.com>
2022-12-20 21:25:42 +0800

    update eslintrc

--- rate-repository-app/.eslintrc.js
+++ rate-repository-app/.eslintrc.js
@@ -1,14 +1,8 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
   extends: [
-    "eslint:recommended",
     "universe/native",
     "plugin:react-native-a11y/all",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
+    "plugin:jest/recommended",
   ],
   overrides: [
     {
@@ -25,16 +19,5 @@ module.exports = {
       },
     },
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  plugins: ["@typescript-eslint", "prettier"],
-  rules: {},
-  settings: { react: { version: "detect" } },
   ignorePatterns: ["/**/__generated__/"],
 };
