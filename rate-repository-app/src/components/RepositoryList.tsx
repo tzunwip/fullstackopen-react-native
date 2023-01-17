5adced92f8b0d665ab0be042e6973b5b8baf9efd
Tzun Wong <tzun.wong@gmail.com>
2023-01-17 23:58:21 +0800

    add repository list search

--- rate-repository-app/src/components/RepositoryList.tsx
+++ rate-repository-app/src/components/RepositoryList.tsx
@@ -1,7 +1,8 @@
 import { useQuery } from "@apollo/client";
 import { useState } from "react";
 import { FlatList, View, StyleSheet } from "react-native";
-import { Menu, Button } from "react-native-paper";
+import { Menu, Button, Searchbar } from "react-native-paper";
+import { useDebounce } from "use-debounce";
 
 import { gql } from "../__generated__";
 import { GetRepositoriesQueryVariables } from "../__generated__/graphql";
@@ -11,12 +12,17 @@ import RepositoryListItemWrapper from "./RepositoryListItemWrapper";
 import { PrimaryText } from "./Text";
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
   separator: {
     height: 2,
     backgroundColor: "lightgrey",
   },
   menu: {
     alignSelf: "flex-start",
+    paddingVertical: 5,
   },
   menuButton: {
     color: theme.colors.textPrimary,
@@ -29,8 +35,13 @@ const GET_REPOSITORIES = gql(/* GraphQL */ `
   query GetRepositories(
     $orderBy: AllRepositoriesOrderBy
     $orderDirection: OrderDirection
+    $searchKeyword: String
   ) {
-    repositories(orderBy: $orderBy, orderDirection: $orderDirection) {
+    repositories(
+      orderBy: $orderBy
+      orderDirection: $orderDirection
+      searchKeyword: $searchKeyword
+    ) {
       edges {
         node {
           id
@@ -69,37 +80,29 @@ const sortConfig: SortConfig = {
 const RepositoryList = () => {
   const [showMenu, setShowMenu] = useState(false);
   const [sortMode, setSortMode] = useState<SortMode>("latest");
-  const { data, loading } = useQuery(GET_REPOSITORIES, {
-    fetchPolicy: "cache-and-network",
-    variables: sortConfig[sortMode].variables,
-  });
-
-  if (loading) return <PrimaryText>Loading..</PrimaryText>;
-  if (!data?.repositories) return <PrimaryText>No Data...</PrimaryText>;
-
-  const renderItem = ({
-    item,
-  }: {
-    item: typeof data.repositories.edges[number];
-  }) => {
-    return (
-      <RepositoryListItemWrapper id={item.node.id}>
-        <RepositoryItem item={item.node} />
-      </RepositoryListItemWrapper>
-    );
-  };
-
+  const [searchQuery, setSearchQuery] = useState("");
+  const [debouncedSearchQuery] = useDebounce(searchQuery, 500);
   function handleItemPress(sortMode: SortMode) {
     setSortMode(sortMode);
     setShowMenu(false);
   }
 
+  function handleChangeSearch(query: string) {
+    setSearchQuery(query);
+  }
+
   return (
-    <>
+    <View style={styles.container}>
+      <Searchbar
+        placeholder="Search..."
+        onChangeText={handleChangeSearch}
+        value={searchQuery}
+      />
       <View style={styles.menu}>
         <Menu
           visible={showMenu}
           onDismiss={() => setShowMenu(false)}
+          anchorPosition="bottom"
           anchor={
             <Button
               icon={sortConfig[sortMode].icon}
@@ -121,13 +124,46 @@ const RepositoryList = () => {
           />
         </Menu>
       </View>
-      <FlatList
-        data={data.repositories.edges}
-        ItemSeparatorComponent={ItemSeparator}
-        renderItem={renderItem}
-      />
-    </>
+      <List sortMode={sortMode} debouncedSearchQuery={debouncedSearchQuery} />
+    </View>
   );
 };
+
+interface ListProps {
+  sortMode: SortMode;
+  debouncedSearchQuery: string;
+}
+
+function List({ sortMode, debouncedSearchQuery }: ListProps) {
+  const { data, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network",
+    variables: {
+      ...sortConfig[sortMode].variables,
+      searchKeyword: debouncedSearchQuery,
+    },
+  });
+
+  if (loading) return <PrimaryText>Loading...</PrimaryText>;
+  if (!data || data?.repositories.edges.length === 0)
+    return <PrimaryText>No repositories found</PrimaryText>;
+
+  const renderItem = ({
+    item,
+  }: {
+    item: typeof data.repositories.edges[number];
+  }) => (
+    <RepositoryListItemWrapper id={item.node.id}>
+      <RepositoryItem item={item.node} />
+    </RepositoryListItemWrapper>
+  );
+
+  return (
+    <FlatList
+      data={data.repositories.edges}
+      ItemSeparatorComponent={ItemSeparator}
+      renderItem={renderItem}
+    />
+  );
+}
 
 export default RepositoryList;
