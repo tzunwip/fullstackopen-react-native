022ce76017cce346c7ced666074324373d362b98
Tzun Wong <tzun.wong@gmail.com>
2023-01-18 15:55:31 +0800

    fix

---
+++ rate-repository-app/src/hooks/useRepository.ts
@@ -0,0 +1,54 @@
+import { useQuery } from "@apollo/client";
+
+import { gql } from "../__generated__";
+import { GetRepositoryQueryVariables } from "../__generated__/graphql";
+
+const GET_REPOSITORY = gql(/* GraphQL */ `
+  query GetRepository($id: ID!, $after: String) {
+    repository(id: $id) {
+      ...RepositoryProfile
+      reviews(after: $after) {
+        edges {
+          node {
+            id
+            ...RepositoryReview
+          }
+        }
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+      }
+    }
+  }
+`);
+
+function useRepository(variables: GetRepositoryQueryVariables) {
+  const { data, loading, fetchMore, ...result } = useQuery(GET_REPOSITORY, {
+    variables,
+    fetchPolicy: "cache-and-network",
+  });
+
+  function handleFetchMore() {
+    const canFetchMore =
+      !loading && data?.repository?.reviews.pageInfo.hasNextPage;
+
+    if (canFetchMore) {
+      fetchMore({
+        variables: {
+          ...variables,
+          after: data.repository?.reviews.pageInfo.endCursor,
+        },
+      });
+    }
+  }
+
+  return {
+    data,
+    loading,
+    fetchMore: handleFetchMore,
+    ...result,
+  };
+}
+
+export default useRepository;
